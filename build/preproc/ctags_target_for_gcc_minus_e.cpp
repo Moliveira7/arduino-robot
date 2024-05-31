8e39e6079125b9869f01b5e64c026e0ee37641d9
Moliveira7 <m7oliveiraf@gmail.com>
2024-05-30 22:15:21 -0300

    Primeiro commit do código Arduino

---
+++ build/preproc/ctags_target_for_gcc_minus_e.cpp
@@ -0,0 +1,7 @@
+# 1 "c:\\Users\\mkyzb\\Documents\\Programacao_e_Cia\\C++\\sketch.ino"
+{
+  "board": "arduino:avr:uno",
+  "sketch": "src/robot.ino",
+  "port": "COM3",
+  "output": "build"
+}
---
+++ build/sketch/sketch.ino.cpp
@@ -0,0 +1,9 @@
+#include <Arduino.h>
+#line 1 "c:\\Users\\mkyzb\\Documents\\Programacao_e_Cia\\C++\\sketch.ino"
+{
+  "board": "arduino:avr:uno",
+  "sketch": "src/robot.ino",
+  "port": "COM3",
+  "output": "build"
+}
+
